bca0034e8a937d7a5f9fd336fce391fecafeba17
zukaishi <ishizuka215@gmail.com>
2021-12-13 08:13:09 +0900

    update

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const fs = require("fs")
 const app = express()
 const port = 3000
 
-// http://localhost:3000/ でresponce.jsonの中身を返す。
 app.get("/", (req, res, next) => {
-    // アクセスログ
-    console.log(req.method, req.url) 
-    // jsonを返す
-    res.json(require('./output.json'))
-});
+    var obj = JSON.parse(fs.readFileSync('./output.json', 'utf8'));
+    var sTranslated = obj.TranslatedText
+    sTranslated = sTranslated.replace("。", "。 ¥r¥n")
+    res.json(sTranslated)
+}); 
+
+app.get("/input", (req, res, next) => {
+    fs.writeFileSync( "input.txt" , "ccccc" )
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.write('OK');
+    res.end();
+}); 
 
 // ポート3000でサーバ起動
 app.listen(port, () => console.log(`click http://localhost:${port}/ !`))
