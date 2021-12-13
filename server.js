89bd95c92f19a51a7317f8b72bb362ea1c467ad3
zukaishi <ishizuka215@gmail.com>
2021-12-14 05:51:45 +0900

    update

--- server.js
+++ server.js
@@ -3,19 +3,26 @@ const fs = require("fs")
 const app = express()
 const port = 3000
 
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) =>{
+    res.sendFile(`${__dirname}/index.html`);
+    });
+
+app.get("/output", (req, res, next) => {
     var obj = JSON.parse(fs.readFileSync('./output.json', 'utf8'));
     var sTranslated = obj.TranslatedText
-    sTranslated = sTranslated.replace("。", "。 ¥r¥n")
-    res.json(sTranslated)
-}); 
+    sTranslated = sTranslated.replace("。", "。\n")
+    sTranslated = sTranslated.replace("、", "、\n")
+    // res.json(sTranslated)
+    res.writeHead(200, { 'Content-Type': 'text/plain;charset=UTF-8'});
+    res.write(sTranslated)
+    res.end();
+});
 
 app.get("/input", (req, res, next) => {
     fs.writeFileSync( "input.txt" , "ccccc" )
-    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.writeHead(200, {'Content-Type' : 'text/plain;charset=UTF-8'});
     res.write('OK');
     res.end();
 }); 
 
-// ポート3000でサーバ起動
 app.listen(port, () => console.log(`click http://localhost:${port}/ !`))
